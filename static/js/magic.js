33f9ebb454facb61d45fb1c90d335899933733dd
Matthew Ebeweber <mattebeweber@utexas.edu>
2014-03-19 18:41:41 -0500

    initial project, webcam access and beginning of flow.

---
+++ static/js/magic.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+    // Hide everything else initially
+    $(".countdown-number").hide();
+
+    // Attach Events
+    $(".start-button").click(function() {
+        beginCountdown();
+    });
+
+
+
+    // Fire off countdown
+    var beginCountdown = function() {
+        console.log("fireOffEventSequence() called.");
+
+        // Hide the take photo button
+        $('.start-button').hide();
+
+        // Count down.
+        $('.three').show();         // 3 ..
+        $('.three').bigText();
+        setTimeout(goToTwo, 1000);
+    };
+
+    // 2 ..
+    var goToTwo = function() {
+        $('.three').hide();
+        $('.two').show();
+        $('.two').bigText();
+        setTimeout(goToOne, 1000);
+    };
+
+    // 1 ..
+    var goToOne = function() {
+        $('.two').hide();
+        $('.one').show();
+        $('.one').bigText();
+        setTimeout(goToGo, 1000);
+    };
+
+    var goToGo = function() {
+        $('.one').hide();
+        $('.go').show();
+        $('.go').bigText();
+    };
+
+    // Web Cam Logic
+    video = document.querySelector('video');
+
+    navigator.webkitGetUserMedia({video:true},
+        function(stream) {
+            video.src = window.webkitURL.createObjectURL(stream);
+        },
+        function(err) {
+            console.log("Unable to get video stream!");
+        }
+    );
+
+      // function snap() {
+      //   live = document.getElementById("live")
+      //   snapshot = document.getElementById("snapshot")
+      //   filmroll = document.getElementById("filmroll")
+
+      //   // Make the canvas the same size as the live video
+      //   snapshot.width = live.clientWidth
+      //   snapshot.height = live.clientHeight
+
+      //   // Draw a frame of the live video onto the canvas
+      //   c = snapshot.getContext("2d")
+      //   c.drawImage(live, 0, 0, snapshot.width, snapshot.height)
+
+      //   // Create an image element with the canvas image data
+      //   img = document.createElement("img")
+      //   img.src = snapshot.toDataURL("image/png")
+      //   img.style.padding = 5
+      //   img.width = snapshot.width / 2
+      //   img.height = snapshot.height / 2
+
+      //   // Add the new image to the film roll
+      //   filmroll.appendChild(img)
+      // }
+});
